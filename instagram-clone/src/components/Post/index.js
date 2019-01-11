5038b79dd95dd58bece44d5f8cc554335f6bd753
Shruti Nanajkar <nanajkar.s@husky.neu.edu>
2019-01-11 16:44:29 -0500

    modified App.js and created index.js in Post

--- instagram-clone/src/components/Post/index.js
+++ instagram-clone/src/components/Post/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import "./Post.css";
+
     class Post extends Component {
       render() {
         return <article className="Post" ref="Post">
             <header>
               <div className="Post-user">
                 <div className="Post-user-avatar">
-                  <img src="https://www.laravelnigeria.com/img/chris.jpg" alt="Chris" />
+                  <img src="user.png" alt="Chris" />
                 </div>
                 <div className="Post-user-nickname">
                   <span>Chris</span>
@@ -14,7 +16,7 @@ import React, { Component } from "react";
             </header>
             <div className="Post-image">
               <div className="Post-image-bg">
-                <img alt="Icon Living" src="https://pbs.twimg.com/media/DOXI0IEXkAAkokm.jpg" />
+                <img alt="Icon Living" src="bali.jpg" />
               </div>
             </div>
             <div className="Post-caption">
